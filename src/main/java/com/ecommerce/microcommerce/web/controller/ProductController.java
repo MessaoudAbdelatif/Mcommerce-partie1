cd58ef575f7b3fbba18a387a480755ad98d1a9a4
MessaoudAbdelatif <dzdevsoft@gmail.com>
2019-10-09 17:11:50 +0200

    Validation du prix de vente

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -65,10 +66,10 @@ public class ProductController {
 
     Product produit = productDao.findById(id);
 
-      if (produit == null) {
-          throw new ProduitIntrouvableException(
-              "Le produit avec l'id " + id + " est INTROUVABLE. Écran Bleu si je pouvais.");
-      }
+    if (produit == null) {
+      throw new ProduitIntrouvableException(
+          "Le produit avec l'id " + id + " est INTROUVABLE. Écran Bleu si je pouvais.");
+    }
 
     return produit;
   }
@@ -78,63 +79,62 @@ public class ProductController {
   @PostMapping(value = "/Produits")
 
   public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
-
-    Product productAdded = productDao.save(product);
+    if (product.getPrix() == 0) {
+      throw new ProduitGratuitException("Le prix de vente ne doit pas être égale à 0");}
+      Product productAdded = productDao.save(product);
 
       if (productAdded == null) {
-          return ResponseEntity.noContent().build();
+        return ResponseEntity.noContent().build();
       }
 
-    URI location = ServletUriComponentsBuilder
-        .fromCurrentRequest()
-        .path("/{id}")
-        .buildAndExpand(productAdded.getId())
-        .toUri();
+      URI location = ServletUriComponentsBuilder
+          .fromCurrentRequest()
+          .path("/{id}")
+          .buildAndExpand(productAdded.getId())
+          .toUri();
 
-    return ResponseEntity.created(location).build();
-  }
-
-  @DeleteMapping(value = "/Produits/{id}")
-  public void supprimerProduit(@PathVariable int id) {
-
-    productDao.delete(id);
-  }
-
-  @PutMapping(value = "/Produits")
-  public void updateProduit(@RequestBody Product product) {
-
-    productDao.save(product);
-  }
-
-
-  //Pour les tests
-  @GetMapping(value = "test/produits/{prix}")
-  public List<Product> testeDeRequetes(@PathVariable int prix) {
-
-    return productDao.chercherUnProduitCher(400);
-  }
-
-
-  //calculerMargeProduit
-  @GetMapping(value = "/AdminProduits")
-  public Map<Product, Integer> calculerMargeProduit() {
-    List<Product> produits = productDao.findAll();
-    HashMap<Product, Integer> marges = new HashMap<>();
-    int marge;
-    for (Product p : produits
-    ) {
-      marge = p.getPrix() - p.getPrixAchat();
-      marges.put(p, marge);
+      return ResponseEntity.created(location).build();
     }
-    return marges;
+
+    @DeleteMapping(value = "/Produits/{id}")
+    public void supprimerProduit ( @PathVariable int id){
+
+      productDao.delete(id);
+    }
+
+    @PutMapping(value = "/Produits")
+    public void updateProduit (@RequestBody Product product){
+
+      productDao.save(product);
+    }
+
+    //Pour les tests
+    @GetMapping(value = "test/produits/{prix}")
+    public List<Product> testeDeRequetes ( @PathVariable int prix){
+
+      return productDao.chercherUnProduitCher(400);
+    }
+
+    //calculerMargeProduit
+    @GetMapping(value = "/AdminProduits")
+    public Map<Product, Integer> calculerMargeProduit () {
+      List<Product> produits = productDao.findAll();
+      HashMap<Product, Integer> marges = new HashMap<>();
+      int marge;
+      for (Product p : produits
+      ) {
+        marge = p.getPrix() - p.getPrixAchat();
+        marges.put(p, marge);
+      }
+      return marges;
+    }
+
+    // Tri par ordre alphabétique
+    @GetMapping(value = "/AdminProduitsByNom")
+    public List<Product> trierProduitsParOrdreAlphabetique () {
+
+      return productDao.findAllByOrderByNomAsc();
+    }
+
+
   }
-
-  // Tri par ordre alphabétique
-  @GetMapping(value = "/AdminProduitsByNom")
-  public List<Product> trierProduitsParOrdreAlphabetique() {
-
-    return productDao.findAllByOrderByNomAsc();
-  }
-
-
-}
